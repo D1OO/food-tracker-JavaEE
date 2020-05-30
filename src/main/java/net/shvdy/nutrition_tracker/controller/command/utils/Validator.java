4a0ee9ee690e1961f7515194c954d23bdb534b53
D1OO <flylikearocket@gmail.com>
2020-05-30 21:43:23 +0300

    Validator can check number fields on min and max values

--- src/main/java/net/shvdy/nutrition_tracker/controller/command/utils/Validator.java
+++ src/main/java/net/shvdy/nutrition_tracker/controller/command/utils/Validator.java
@@ -2,7 +2,9 @@ package net.shvdy.nutrition_tracker.controller.command.utils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 27.05.2020
@@ -14,8 +16,19 @@ public class Validator {
 
     public static Map<String, String> validateFormAndReturnErrors(HttpServletRequest request,
                                                                   Map<String, Map<String, String>> fieldsValidationData) {
-        return fieldsValidationData.entrySet().stream()
-                .filter(field -> !request.getParameter(field.getKey()).matches((field.getValue().get("regex"))))
-                .collect(Collectors.toMap(field -> field.getKey() + "Error", field -> field.getValue().get("errorMessage")));
+        return Stream.of(
+                fieldsValidationData.entrySet().stream()
+                        .filter(field -> !request.getParameter(field.getKey()).matches((field.getValue().get("regex")))),
+                fieldsValidationData.entrySet().stream()
+                        .filter(field -> Optional.ofNullable(field.getValue().get("max")).isPresent())
+                        .filter(field -> Double.parseDouble(request.getParameter(field.getKey())) >
+                                Double.parseDouble(field.getValue().get("max"))),
+                fieldsValidationData.entrySet().stream()
+                        .filter(field -> Optional.ofNullable(field.getValue().get("min")).isPresent())
+                        .filter(field -> Double.parseDouble(request.getParameter(field.getKey())) <
+                                Double.parseDouble(field.getValue().get("min"))))
+                .flatMap(i -> i)
+                .collect(Collectors.toMap(field -> field.getKey() + "Error", field -> field.getValue().get("errorMessage"),
+                        (duplicate1, duplicate2) -> duplicate1));
     }
 }
